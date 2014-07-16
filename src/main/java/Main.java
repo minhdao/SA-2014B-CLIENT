dde804c1b122408ea54ca8e754706d68563096e6
crazywin3 <crazywin3@gmail.com>
2014-07-16 18:57:12 +0700

    Implement JFrame

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,36 @@
+import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 
 /**
  * Created by minh on 7/16/14.
  */
-public class Main {
+public class Main extends JFrame{
     private static LinkedHashMap<Integer, String> cardMap = new LinkedHashMap<Integer, String>();
     private static ArrayList<Integer> cardDeck;
 
+    private JButton play = new JButton("Play game");
+
     public static void main(String[] args) {
         setCardMap();
+        Main main = new Main();
+    }
+
+    public Main(){
+        add(new JLabel(new ImageIcon("tienlen.jpg")), BorderLayout.CENTER);
+
+        JPanel menu = new JPanel(new FlowLayout());
+        menu.setBackground(Color.black);
+
+        menu.add(play);
+        add(menu, BorderLayout.SOUTH);
+        setTitle("TIEN LEN - Client");
+        setVisible(true);
+        setSize(600, 400);
+        setLocation(100, 100);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pack();
     }
 
     private static void setCardMap(){
