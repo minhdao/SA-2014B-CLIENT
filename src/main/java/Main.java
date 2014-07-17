69dd26f82a9cb3560c1aaf333add673059790ecf
crazywin3 <crazywin3@gmail.com>
2014-07-17 17:03:44 +0700

    InputName

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,24 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.Scanner;
 
 /**
  * Created by minh on 7/16/14.
  */
-public class Main extends JFrame{
+public class Main extends JFrame implements ActionListener {
     private static LinkedHashMap<Integer, String> cardMap = new LinkedHashMap<Integer, String>();
     private static ArrayList<Integer> cardDeck;
 
     private JButton play = new JButton("Play game");
+    private JTextField name = new JTextField();
 
     public static void main(String[] args) {
         setCardMap();
@@ -18,19 +26,24 @@ public class Main extends JFrame{
     }
 
     public Main(){
-        add(new JLabel(new ImageIcon("tienlen.jpg")), BorderLayout.CENTER);
-
+        JPanel banner = new JPanel(new FlowLayout());
+        banner.add(new JLabel(new ImageIcon("img/tienlen.jpg")));
+        //banner.setBackground(Color.BLACK);
         JPanel menu = new JPanel(new FlowLayout());
         menu.setBackground(Color.black);
 
+        name.setColumns(20);
+        play.addActionListener(this);
+        menu.add(name);
         menu.add(play);
         add(menu, BorderLayout.SOUTH);
+        add(banner, BorderLayout.CENTER);
+
         setTitle("TIEN LEN - Client");
         setVisible(true);
         setSize(600, 400);
         setLocation(100, 100);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        pack();
     }
 
     private static void setCardMap(){
@@ -45,5 +58,26 @@ public class Main extends JFrame{
     // Display cards from server
     private static void getCards(ArrayList<Integer> cards){
         cardDeck.addAll(cards);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        JButton b = (JButton)ae.getSource();
+        if(b == play){
+            try {
+                Socket client = new Socket("localhost",18888);
+                DataInputStream dis = new DataInputStream(client.getInputStream());
+                DataOutputStream dos = new DataOutputStream(client.getOutputStream());
+
+                String input = name.getText();
+
+                dos.writeUTF(input);
+                //dis.readUTF();
+                System.out.println(dis.readUTF());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            //dispose();
+        }
     }
 }
