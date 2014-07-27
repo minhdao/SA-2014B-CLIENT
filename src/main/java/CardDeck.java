034d7fe0196cd509e58a23e5d8bc93170b07d811
Minh Dao <minhdao6@gmail.com>
2014-07-27 18:42:03 +0700

    added deckType property

--- src/main/java/CardDeck.java
+++ src/main/java/CardDeck.java
@@ -5,10 +5,15 @@ import java.util.ArrayList;
  * Created by Shinichi on 21/07/2014.
  */
 public class CardDeck implements Serializable {
+    private Status deckType;
+
     private ArrayList<Integer> cards = new ArrayList<Integer>();
 
     public ArrayList<Integer> getCards() {
         return cards;
     }
 
+    public Status getDeckType() {
+        return deckType;
+    }
 }
