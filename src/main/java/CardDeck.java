a3cbac84baf481d63807af975aa7a74fd7434b54
Minh Dao <minhdao6@gmail.com>
2014-07-27 18:49:24 +0700

    removed deckType

--- src/main/java/CardDeck.java
+++ src/main/java/CardDeck.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
  * Created by Shinichi on 21/07/2014.
  */
 public class CardDeck implements Serializable {
-    private Status deckType;
 
     private ArrayList<Integer> cards = new ArrayList<Integer>();
 
@@ -13,7 +12,4 @@ public class CardDeck implements Serializable {
         return cards;
     }
 
-    public Status getDeckType() {
-        return deckType;
-    }
 }
